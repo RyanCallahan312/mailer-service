bcc218931ed9b65f5fa0a3cb713f30dc5b1de827
Ryan Callahan <RyanCallahan312@gmail.com>
2019-08-29 16:40:05 -0400

    update
    update

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const exphbs = require('express-handlebars');
 const nodemailer = require('nodemailer');
 
 const app = express();
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-	res.send('Hello');
+app.post('/api/mailer', async (req, res) => {
+	try {
+		const { mailData, mailerInfo } = req.body;
+		let transporter = nodemailer.createTransport(mailerInfo);
+		let mail = await transporter.sendMail(mailData);
+		response = { message: 'Mail Sent', mailData: mail };
+	} catch (e) {
+		response = { message: 'Mail Failed', error: e };
+	}
+	console.log(response)
+	res.send(response);
 });
 
-app.listen(8001, () => console.log('Server started...'));
+const PORT = process.env.PORT || 8001;
+
+app.listen(PORT, () => console.log('Server started...'));
