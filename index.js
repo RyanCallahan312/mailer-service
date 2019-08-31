06a132713bc2abf5565805880431384b665dcd18
Ryan Callahan <RyanCallahan312@gmail.com>
2019-08-31 10:45:02 -0400

    d

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/api/mailer', async (req, res) => {
-	try {
-		const { mailData, mailerInfo } = req.body;
-		let transporter = nodemailer.createTransport(mailerInfo);
-		let mail = await transporter.sendMail(mailData);
-		response = { message: 'Mail Sent', mailData: mail };
-	} catch (e) {
-		response = { message: 'Mail Failed', error: e };
-	}
-	console.log(response)
-	res.send(response);
+app.post("/api/mailer", async (req, res) => {
+  try {
+    const { mailData, mailerInfo } = req.body;
+    let transporter = nodemailer.createTransport(mailerInfo);
+    let mail = await transporter.sendMail(mailData);
+    response = { message: "Mail Sent", mailData: mail };
+
+    res.status(200).json(response);
+  } catch (e) {
+    response = { message: "Mail Failed", error: e };
+    res.status(503).json(response);
+  }
+  console.log(response);
 });
 
 const PORT = process.env.PORT || 8001;
 
-app.listen(PORT, () => console.log('Server started...'));
+app.listen(PORT, () => console.log("Server started..."));
