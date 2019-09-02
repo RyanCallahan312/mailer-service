1919ac716448d1b4252e3b5181af1ba674912e00
Ryan Callahan <RyanCallahan312@gmail.com>
2019-09-02 12:39:56 -0400

    k

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(cors())
 
