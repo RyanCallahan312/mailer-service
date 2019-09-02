01eedc6168a5d256c628624fb5c7f7c3a785ddc6
Ryan Callahan <RyanCallahan312@gmail.com>
2019-09-02 12:22:37 -0400

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const cors = require("cors");
 const nodemailer = require("nodemailer");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(cors())
 
 app.post("/api/mailer", async (req, res) => {
   try {
