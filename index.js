6768f0abf74bbceaad65640d6b0216b10ac2a51b
Ryan Callahan <RyanCallahan312@gmail.com>
2019-09-03 23:09:32 -0400

    formatting

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(cors())
+app.use(cors());
 
 app.post("/api/mailer", async (req, res) => {
-  try {
-    const { mailData, mailerInfo } = req.body;
-    let transporter = nodemailer.createTransport(mailerInfo);
-    let mail = await transporter.sendMail(mailData);
-    response = { message: "Mail Sent", mailData: mail };
+    try {
+        const { mailData, mailerInfo } = req.body;
+        let transporter = nodemailer.createTransport(mailerInfo);
+        let mail = await transporter.sendMail(mailData);
+        response = { message: "Mail Sent", mailData: mail };
 
-    res.status(200).json(response);
-  } catch (e) {
-    response = { message: "Mail Failed", error: e };
-    res.status(503).json(response);
-  }
-  console.log(response);
+        res.status(200).json(response);
+    } catch (e) {
+        response = { message: "Mail Failed", error: e };
+        res.status(503).json(response);
+    }
+    console.log(response);
 });
 
 const PORT = process.env.PORT || 8080;
